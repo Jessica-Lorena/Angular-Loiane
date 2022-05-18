6503f47f452289ee34cb0f62c65af7b6a307a745
Jessica-Lorena <jessicacslorena@gmail.com>
2022-05-18 14:57:23 -0300

    event-binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,9 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem = 'https://picsum.photos/400/200';
+  valorSalvo: any = '';
+  valorAtual: any = '';
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -17,6 +20,22 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+  
+  botaoClicado(){
+    alert()
+  }
+   
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value)
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
   }
 
   constructor() { }
